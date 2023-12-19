0ed44e04eb83a26f0eb0ce7d170efb76f56d3246
Adel Hadadi <hadadiadel81@gmail.com>
2023-12-19 15:44:21 +0330

    remove comments

--- cmd/model.go
+++ cmd/model.go
@@ -23,14 +23,4 @@ var modelCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(modelCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// handlerCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// handlerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
